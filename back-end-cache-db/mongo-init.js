0a4bde1fbedd7f62f8081a0243f4e43a70aa72c3
Django Shelton <djangoshelton@gmail.com>
2018-11-28 16:13:16 +0000

    initial commit

---
+++ back-end-cache-db/mongo-init.js
@@ -0,0 +1,12 @@
+db = db.getSiblingDB('register_a_food_business_cache');
+db.createUser({
+  user: 'test-user',
+  pwd: 'test-password',
+  roles: [
+    {
+      role: 'root',
+      db: 'admin',
+    },
+  ],
+});
+db.createCollection('cachedRegistrations');
---
+++ cloneRepos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-service.git
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-front-end.git
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-collections-service.git
