f50023093a5be2bbcd51766280ddb155ad6d3002
Simon Thomas <simon.thomas@civica.co.uk>
2020-01-21 14:05:28 +0000

    make all repos siblings of this one

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-git clone https://github.com/FoodStandardsAgency/register-a-food-business-service.git
-git clone https://github.com/FoodStandardsAgency/register-a-food-business-front-end.git
-git clone https://github.com/FoodStandardsAgency/register-a-food-business-collections-service.git
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-service.git  ../
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-front-end.git ../ 
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-collections-service.git ../
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-validation.git ../
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-healthcheck-dashboard.git ../
+git clone https://github.com/FoodStandardsAgency/register-a-food-business-performance-tests.git ../
