a7ff992f19e7720d13139a2f93218613ca6b4378
Adam White <awhite234@gmail.com>
2020-04-08 15:23:11 +0100

    Tweaks for npm work.

--- bootstrap-npm/npminit.sh
+++ bootstrap-npm/npminit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-npm config set -g proxy "http://docker.for.win.localhost:3128" && \
-npm config set -g https-proxy "http://docker.for.win.localhost:3128" && \
 npm config set -g progress=false && \
 npm config set -g strict-ssl false && \
 npm config set -g cache /opt/npmcache && \
@@ -8,4 +6,5 @@ npm config set -g registry https://registry.npmjs.org/ && \
 npm config set -g '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}" && \
 npm config ls --long
 
+tail -f /dev/null
 
