41d3db192e8035891c1a830b15e7b6999c96ec61
Adam White <awhite234@gmail.com>
2020-04-29 15:48:27 +0100

    Update to remove npm containers. Speak to ANW for instructions / problems.

--- bootstrap-npm-cache/npminit.sh
+++ bootstrap-npm-cache/npminit.sh
@@ -7,8 +7,8 @@
     cd /usr/src/rafbfe &&
     rm -f .npmrc &&
 
-    npm config set -g proxy "http://docker.for.win.localhost:3128" && \
-    npm config set -g https-proxy "http://docker.for.win.localhost:3128" && \
+    npm config set -g proxy "${DOCKER_HTTP_PROXY}" && \
+    npm config set -g https-proxy "${DOCKER_HTTPS_PROXY}" && \
     npm config set -g progress=false && \
     npm config set -g strict-ssl false && \
     npm config set -g cache /opt/npmcache && \
